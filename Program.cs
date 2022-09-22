32b80ee453e7e14df8729340162ed9339b388fe8
Alex Vestelind <alex_vestelind@hotmail.com>
2022-09-22 11:07:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace testing_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool reset = false;
+            do
+            {
+                try
+                {
+                    
+                    calc calc = new calc();
+                    double mathInput1 = 0;
+                    double mathInput2 = 0;
+                    Console.Clear();
+                    Console.WriteLine("Please choose a number");
+                    Console.WriteLine("1. Addition");
+                    Console.WriteLine("2. Subtraction");
+                    Console.WriteLine("3. Multiplication");
+                    Console.WriteLine("4. Division");
+                    Console.WriteLine("9. Exit");
+                    int menuNum = int.Parse(Console.ReadLine());
+
+                    switch (menuNum)
+                    {
+                        case 1:
+                            Console.WriteLine("enter one number at a time");
+                            mathInput1 = double.Parse(Console.ReadLine());
+                            Console.WriteLine("+");
+                            mathInput2 = double.Parse(Console.ReadLine());
+                            calc.Addition(mathInput1, mathInput2);
+                            Console.WriteLine("{0} + {1} = {2}", mathInput1, mathInput2, calc.sum);
+                            break;
+                        case 2:
+                            Console.WriteLine("enter one number at a time");
+                            mathInput1 = double.Parse(Console.ReadLine());
+                            Console.WriteLine("-");
+                            mathInput2 = double.Parse(Console.ReadLine());
+                            calc.Subtraction(mathInput1, mathInput2);
+                            Console.WriteLine("{0} - {1} = {2}", mathInput1, mathInput2, calc.sum);
+                            break;
+                        case 3:
+                            Console.WriteLine("enter one number at a time");
+                            mathInput1 = double.Parse(Console.ReadLine());
+                            Console.WriteLine("*");
+                            mathInput2 = double.Parse(Console.ReadLine());
+                            calc.Multiplication(mathInput1, mathInput2);
+                            Console.WriteLine("{0} * {1} = {2}", mathInput1, mathInput2, calc.sum);
+                            break;
+                        case 4:
+                            Console.WriteLine("enter one number at a time");
+                            mathInput1 = double.Parse(Console.ReadLine());
+                            Console.WriteLine("/");
+                            mathInput2 = double.Parse(Console.ReadLine());
+                            calc.Division(mathInput1, mathInput2);
+                            Console.WriteLine("{0} / {1} = {2}", mathInput1, mathInput2, calc.sum);
+                            break;
+                        case 9:
+                            Environment.Exit(1);
+                            break;
+                        default:
+                            Console.WriteLine("please enter a valid number");
+                            break;
+                    }
+
+                }
+                catch (Exception ex)
+                { 
+                    Console.WriteLine("Error: {0}",ex);
+                }
+                Console.WriteLine("press any key to continue");
+                Console.ReadKey();
+            } while (reset == false);
+        }
+    }
+}
---
+++ calc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_calculator
+{
+    public class calc
+    {
+        public double sum = 0;
+        public double Addition(double num1, double num2)
+        {
+            sum = num1 + num2;
+            return sum;
+        }
+        public double Addition(int[] intArray)
+        {
+            
+            foreach (int num in intArray)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+        public double Subtraction(double num1, double num2)
+        {
+            sum = num1 - num2;
+            return sum;
+        }
+        public double Subtraction(double[] doubleArray)
+        {
+
+            for (int i = 1; i < doubleArray.Length; i++)
+            {
+                sum = doubleArray[0] -= doubleArray[i];
+            }
+            return sum;
+        }
+        public double Multiplication(double num1, double num2)
+        {
+            sum = (num1 * num2);
+            return sum;
+        }
+        public double Division(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            sum = num1 / num2;
+            return sum;
+        }
+        public void Reset()
+        {
+            sum = 0;
+        }
+    }
+}
